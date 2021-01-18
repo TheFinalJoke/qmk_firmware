3ef9f51252eb4534a0be09fd12a40299b00e4ebc
Nicholas Shorter <nickshorter@Nicholass-MacBook-Pro.local>
2021-01-18 14:24:39 -0600

    add light sabr

--- keyboards/lily58/keymaps/thefinaljoke/keymap.c
+++ keyboards/lily58/keymaps/thefinaljoke/keymap.c
@@ -143,7 +143,7 @@ static void render_status(void) {
     oled_write_P(PSTR("      Layer"), false);
     switch (get_highest_layer(layer_state)) {
         case _QWERTY:
-            oled_write_P(PSTR("Def"), false);
+            oled_write_P(PSTR("Def  "), false);
             break;
         case _LOWER:
             oled_write_P(PSTR("Lower"), false);
@@ -157,12 +157,6 @@ static void render_status(void) {
         default:
             oled_write_P(PSTR("Undefined"), false);
     }
-
-    //Host Keyboard LED Status
-    led_t led_state = host_keyboard_led_state();
-    oled_write_P(led_state.num_lock ? PSTR("NUM ") : PSTR("       "), false);
-    oled_write_P(led_state.caps_lock ? PSTR("CAPS ") : PSTR("       "), false);
-    oled_write_P(led_state.scroll_lock ? PSTR("SCRL") : PSTR("       "), false);
 }
 
 
@@ -284,13 +278,37 @@ static void render_anim(void) {
         }
     }
 }
-
+    void lightsaber(void){
+        oled_write_P(PSTR("     "), false);
+        oled_write_P(PSTR("====="), false);
+        oled_write_P(PSTR("|   |"), false);
+        oled_write_P(PSTR("\\---/"), false);
+        oled_write_P(PSTR(" |-| "), false);
+        oled_write_P(PSTR("/---\\"), false);
+        oled_write_P(PSTR("| | |"), false);
+        oled_write_P(PSTR("| | |"), false);
+        oled_write_P(PSTR("|_|_|"), false);
+                /*
+                |    |
+                |    |
+                \---/
+                 |-|
+                 |-|
+                /---\
+                | | |
+                | | |
+                | | |
+                |_|_|
+                ")
+                */
+    }
 void oled_task_user(void) {
     if (is_keyboard_master()) {
         oled_write_P(PSTR("HelloThe  FinalJoke"), false);
         //render_skull();
         //oled_set_cursor(7,6);
         render_status();
+        lightsaber();
 
      // Renders the current keyboard state (layer, lock, caps, scroll, etc)
     } else {
